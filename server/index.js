e2664b899ad9feaf5e5d2f1f43c0a9082a207242
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2021-02-04 18:30:38 -0600

    Client and Server talking

--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
 const db = mysql.createPool({
-    host: 'hostNameGoesHere',
-    user: 'userNameGoesHere',
-    password:'passwordGoesHere',
-    database:'nameOfDataBaseGoesHere',
+    host: 'yourHostHere',
+    user: 'yourUserHere',
+    password:'yourPasswordHere',
+    database:'yourDatabaseHere',
 });
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
+app.get('/api/get', (req, res)=>{
+    const sqlSelect = "SELECT * FROM movie_reviews";
+    db.query(sqlSelect, (err, result)=>{
+        res.send(result)
+    });
+})
 
-app.get("/", (req, res) => {})
+app.post('/api/insert', (req, res)=>{
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)"
+    db.query(sqlInsert, [movieName, movieReview], (err, result)=>{
+        console.log(result);
+    });
+});
 
 app.listen(3001, ()=>{
     console.log('running on port 3001')
